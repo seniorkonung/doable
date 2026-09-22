ce75db42d22381f7118d54c0bd48871442a10d70
Valentin Panchenko <senior.konung@gmail.com>
2026-09-22 21:24:51 +0300

    chore(relations): confirm phase 5 lifecycle readiness

--- lib/src/intention/presentation/details/intention_details_view_model.g.dart
+++ lib/src/intention/presentation/details/intention_details_view_model.g.dart
@@ -150,7 +150,7 @@ final class IntentionDetailsViewModelProvider
 }
 
 String _$intentionDetailsViewModelHash() =>
-    r'61aa29153488982d09472225608389f8594b9518';
+    r'9282993a707f93dc9c9bbd754e1b040af0717db1';
 
 final class IntentionDetailsViewModelFamily extends $Family
     with
